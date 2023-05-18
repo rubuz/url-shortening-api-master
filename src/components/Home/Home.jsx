c8699039b8669f36845c93ae863d2bc3fe37ad40
Matjaz Lovenjak <rubuz23@gmail.com>
2023-05-18 21:12:55 +0200

    header done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import homeImg from "../../assets/illustration-working.svg";
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="section container">
+      <div className="home__container">
+        <div className="home__title">
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand's recognition and get deatailed insights on how
+            your links are performing.
+          </p>
+          <button className="btn">Get started</button>
+        </div>
+        <div className="home__img">
+          <img
+            src={homeImg}
+            alt="human sitting by the desk and working on a computer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
