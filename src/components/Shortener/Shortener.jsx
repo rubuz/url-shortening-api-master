0afa2a3f11c8c5a984c51fd6aab401d8f061131d
rubuz <rubuz23@gmail.com>
2023-07-07 14:57:14 +0200

    starting on statiscits section

--- src/components/Shortener/Shortener.jsx
+++ src/components/Shortener/Shortener.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./shortener.css";
 
 const Shortener = () => {
-  return <div>Shortener</div>;
+  return (
+    <div className="shortener__container container">
+      <div className="shortener__input-container">
+        <div className="shortener__input-box">
+          <input
+            className="shortener__input"
+            type="text"
+            placeholder="Shorten a link here..."
+          />
+          {/* <span>Please add a link</span> */}
+        </div>
+
+        <button className="shortener__btn btn">Shorten it!</button>
+      </div>
+    </div>
+  );
 };
 
 export default Shortener;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./statistics.css";
 
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <section className="section container">
+      <div className="statistics__title-container">
+        <h2>Advanced Statistics</h2>
+        <p>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Statistics;
