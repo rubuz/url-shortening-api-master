0da9c5a28154b08c4fab5b8bc688898c5831ed5b
rubuz <rubuz23@gmail.com>
2023-07-21 12:24:51 +0200

    footer done

--- src/components/Footer/FooterIcons.jsx
+++ src/components/Footer/FooterIcons.jsx
@@ -3,15 +3,16 @@ import fb from "../../assets/icon-facebook.svg";
 import twitter from "../../assets/icon-twitter.svg";
 import pinterest from "../../assets/icon-pinterest.svg";
 import ig from "../../assets/icon-instagram.svg";
+import { ReactSVG } from "react-svg";
 import "./footer.css";
 
 const FooterIcons = () => {
   return (
     <div className="icon__container">
-      <img src={fb} alt="Facebook logo" />
-      <img src={twitter} alt="Twitter logo" />
-      <img src={pinterest} alt="Pinterest logo" />
-      <img src={ig} alt="Instagram logo" />
+      <ReactSVG src={fb} alt="Facebook logo" />
+      <ReactSVG src={twitter} alt="Twitter logo" />
+      <ReactSVG src={pinterest} alt="Pinterest logo" />
+      <ReactSVG src={ig} alt="Instagram logo" />
     </div>
   );
 };
