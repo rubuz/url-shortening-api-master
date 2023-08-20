9bf68c0d4b296188f457444e47a16e3910c213f3
Matjaz Lovenjak <rubuz23@gmail.com>
2023-08-20 10:49:31 +0200

    basic api call done

--- src/components/Shortener/Shortener.jsx
+++ src/components/Shortener/Shortener.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./shortener.css";
+// https://api.shrtco.de/v2/shorten?url=
 
 const Shortener = () => {
+  const [url, setUrl] = useState("");
+  const [link, setLink] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!url) {
+      alert("Input is empty");
+    } else {
+      // Check if it a valid link
+      const getShortLink = async () => {
+        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+        const data = await res.json();
+        console.log(data);
+        setLink(data.result);
+        setUrl("");
+      };
+
+      getShortLink();
+    }
+  };
+
   return (
     <div className="shortener__container container">
-      <div className="shortener__input-container">
+      <form className="shortener__input-container" onSubmit={handleSubmit}>
         <div className="shortener__input-box">
           <input
             className="shortener__input"
-            type="text"
+            type="url"
             placeholder="Shorten a link here..."
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
           />
           {/* <span>Please add a link</span> */}
         </div>
 
-        <button className="shortener__btn btn">Shorten it!</button>
-      </div>
+        <button
+          type="submit"
+          className="shortener__btn btn"
+          onClick={handleSubmit}
+        >
+          Shorten it!
+        </button>
+      </form>
+      <div></div>
     </div>
   );
 };
